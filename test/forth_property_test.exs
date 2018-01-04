ddddb2dce98725cbafb5d479901086066f4cd8ca
Nick Mabry <nicholasmabry@gmail.com>
2018-01-04 11:08:32 -0700

    Minor refactoring.

--- test/forth_property_test.exs
+++ test/forth_property_test.exs
@@ -1,8 +1,8 @@
 defmodule ForthPropertyTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   use ExUnitProperties
 
-  @max_runs 1_000
+  @max_runs 500
   @valid_words ["+",  "-", "*", "/", ":"] ++ ~w[dup drop swap over]
 
   def add_numbers_program_generator do
@@ -15,6 +15,14 @@ defmodule ForthPropertyTest do
     end
   end
 
+  defp invalid_word_program_generator do
+    gen all word <- StreamData.integer(),
+            value <- StreamData.integer(),
+            expected_message = "invalid word: \"#{word}\"" do
+      {expected_message, ": #{word} #{value} ;"}
+    end
+  end
+
   def push_numbers_program_generator do
     gen all numbers <- StreamData.list_of(StreamData.integer()) do
       expected_stack = Enum.join(numbers, " ")
@@ -23,21 +31,31 @@ defmodule ForthPropertyTest do
   end
 
   def word_define_program_generator do
-    gen all label <- StreamData.string(:alphanumeric, min_length: 1),
-            !String.match?(label, ~R/^\d+$/u),
+    gen all word <- StreamData.string(:alphanumeric, min_length: 1),
+            !String.match?(word, ~R/^\d+$/u),
             value <- StreamData.integer() do
       expected_stack = Integer.to_string(value)
-      {expected_stack, ": #{label} #{value} ; #{label}"}
+      {expected_stack, ": #{word} #{value} ; #{word}"}
+    end
+  end
+
+  def word_redefine_program_generator do
+    gen all word <- StreamData.string(:alphanumeric, min_length: 1),
+            !String.match?(word, ~R/^\d+$/u),
+            program = ": #{word} dup ; : #{word} dup dup ; 1 #{word}",
+            expected_stack = "1 1 1" do
+      {expected_stack, program}
     end
   end
 
   def unknown_word_program_generator do
-    gen all word <- StreamData.string(:printable, min_length: 1),
+    gen all word <- StreamData.string(:alphanumeric, min_length: 1),
             !String.match?(word, ~R/^\d+$/u),
-            !(String.downcase(word) in @valid_words),
+            downcased_word = String.downcase(word),
+            !(downcased_word in @valid_words),
             prefix <- StreamData.list_of(StreamData.constant("1 1 + ")),
             suffix <- StreamData.list_of(StreamData.constant(" 1 1 +")),
-            expected_message = "unknown word: \"#{word}\"" do
+            expected_message = "unknown word: \"#{downcased_word}\"" do
       {expected_message, Enum.join(prefix) <> word <> Enum.join(suffix)}
     end
   end
@@ -52,7 +70,17 @@ defmodule ForthPropertyTest do
     end
   end
 
-  property "words may be defined with any alphanumeric characters" do
+  property "words may be defined" do
+    check all {expected_stack, program} <- word_define_program_generator(),
+              max_runs: @max_runs do
+      stack = Forth.new()
+                 |> Forth.eval(program)
+                 |> Forth.format_stack
+      assert stack == expected_stack
+    end
+  end
+
+  property "words may be redefined" do
     check all {expected_stack, program} <- word_define_program_generator(),
               max_runs: @max_runs do
       stack = Forth.new()
@@ -72,7 +100,17 @@ defmodule ForthPropertyTest do
     end
   end
 
-  property "invalid words raise an UnknownWord exception" do
+  property "invalid words raise an InvalidWord exception" do
+    check all {expected_message, program} <- invalid_word_program_generator(),
+              max_runs: @max_runs do
+      assert_raise(Forth.InvalidWord, expected_message, fn () ->
+        Forth.new()
+        |> Forth.eval(program)
+      end)
+    end
+  end
+
+  property "unknown words raise an UnknownWord exception" do
     check all {expected_message, program} <- unknown_word_program_generator(),
               max_runs: @max_runs do
       assert_raise(Forth.UnknownWord, expected_message, fn () ->
